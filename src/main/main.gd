b9cc6a2f3bd8ddcda60997cf2f869ec287f774a2
Dallas Greene <dgreene1641@gmail.com>
2025-09-23 08:46:18 -0400

    add end credits

--- src/main/main.gd
+++ src/main/main.gd
@@ -10,6 +10,7 @@ var world: Node3D
 
 func _ready() -> void:
 	SignalBus.start_game.connect(_on_start_game)
+	EyeHealth.blindness_achieved.connect(_on_blindness_achieved)
 	UI = %UI
 	world = %World
 
@@ -18,3 +19,8 @@ func _on_start_game() -> void:
 	UI.load_game()
 	var world_scene_instance = world_scene.instantiate()
 	world.add_child(world_scene_instance)
+
+
+func _on_blindness_achieved() -> void:
+	UI.show_victory_screen()
+	world.queue_free()
--- src/main/ui.gd
+++ src/main/ui.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+@export var end_credits: PackedScene
+
+
 func _ready() -> void:
 	$OnwardButton.pressed.connect(_on_onward_pressed)
 	$QuitButton.pressed.connect(_on_quit_pressed)
@@ -10,6 +13,14 @@ func load_game() -> void:
 	$OnwardButton.queue_free()
 	$QuitButton.queue_free()
 	$TitleScreenMock.queue_free()
+
+
+func show_victory_screen() -> void:
+	for child in get_children():
+		child.queue_free()
+	var end_credits_scene = end_credits.instantiate()
+	add_child(end_credits_scene)
+	end_credits_scene.find_child("CreditsAnimation").play("credits")
 
 
 func _on_onward_pressed() -> void:
